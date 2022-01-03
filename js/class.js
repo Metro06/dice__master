94dedc421d224540091bf636710d804990646bbf
Metro06 <68827730+Metro06@users.noreply.github.com>
2022-01-03 22:44:03 +0100

    add animation dice and a switch for modified face one

---
+++ js/class.js
@@ -0,0 +1,208 @@
+class PlayerOne {
+  constructor() {
+    this.speudo1 = document.getElementById('player__one--name');
+    this.score1 = document.getElementById('player__one--score');
+    this.currentScore1 = document.getElementById('player__one--currentScore');
+    this.enterName1 = document.getElementById('name1')
+    this.backgroundPlayerOne = document.getElementById('player__one');
+
+  }
+
+  getSpeudo1 = function () {
+    this.speudo1
+  }
+
+  getScore1 = function () {
+    this.score1
+  }
+
+  getCurrentScore1 = function () {
+    this.currentScore1
+  }
+
+  // reset score
+  setResetPlayerOne = function () {
+    this.score1.innerHTML = 0;
+    this.currentScore1.innerHTML = 0
+  }
+
+  // modified spedo
+  setSpeudo1 = function () {
+    this.speudo1.innerHTML = this.enterName1.value;
+  }
+
+
+  // modified background in active
+  setPlayerOneActive = function (g, p1, p2) {
+    if (g.currentTurn === 1) {
+      p1.backgroundPlayerOne.classList.add('is-turn-player-active')
+      p2.backgroundPlayerTwo.classList.remove('is-turn-player-active')
+      p2.backgroundPlayerTwo.classList.add('is-turn-player-off')
+    } else {
+
+    }
+  }
+
+  // modified background in off
+  setAddBackgroundPlayerOneOff = function () {
+    this.backgroundPlayerOne.classList.add('is-turn-player-off')
+  }
+}
+
+// Same for playerTwo
+class PlayerTwo {
+  constructor() {
+    this.speudo2 = document.getElementById('player__two--name');
+    this.score2 = document.getElementById('player__two--score');
+    this.currentScore2 = document.getElementById('player__two--currentScore');
+    this.enterName2 = document.getElementById('name2');
+    this.backgroundPlayerTwo = document.getElementById('player__two');
+  }
+
+  getSpeudo2 = function () {
+    return this.speudo2
+  }
+
+  getScore2 = function () {
+    return this.score2
+  }
+
+  getCurrentScore2 = function () {
+    return this.currentScore2
+  }
+
+  setResetPlayerTwo = function () {
+    this.score2.innerHTML = 0;
+    this.currentScore2.innerHTML = 0;
+  }
+
+
+  setSpeudo2 = function () {
+    this.speudo2.innerHTML = this.enterName2.value;
+  }
+
+
+  setPlayerTwoActive = function (g, p2, p1) {
+    if (g.currentTurn === 2) {
+      p2.backgroundPlayerTwo.classList.add('is-turn-player-active')
+      p1.backgroundPlayerOne.classList.remove('is-turn-player-active')
+      p1.backgroundPlayerOne.classList.add('is-turn-player-off')
+    } else {
+
+    }
+  }
+
+  setAddBackgroundPlayerTwoOff = function () {
+    this.backgroundPlayerTwo.classList.add('is-turn-player-off')
+  }
+
+}
+
+
+class Game {
+
+  constructor() {
+    this.currentTurn = undefined;
+    this.playerWinner = undefined;
+    this.dice = document.getElementById('dice');
+    this.faceDiceRandom = document.getElementById('face__one')
+    this.btnRollDice = document.getElementById('Roll__dice');
+    this.btnHold = document.getElementById('btn__hold');
+
+
+  }
+
+  setBtnAppend = function (g) {
+    g.btnRollDice.classList.remove('is-btn-hidden');
+    g.btnHold.classList.remove('is-btn-hidden');
+  }
+
+  setDebounce = function (callback, delay) {
+    var timer;
+    return function () {
+      var args = arguments;
+      var context = this;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        callback.apply(context, args);
+      }, delay)
+    }
+  }
+
+  setRandomNumber = function (g, p1, p2) {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    let addCurrentScore = randomNumber;
+    addCurrentScore += currentScore;
+
+
+
+    console.log(currentScore)
+
+      switch (randomNumber) {
+        case 1:
+          this.faceDiceRandom.style.backgroundImage =
+            "url(../image/dice-six-faces-one.svg)";
+          break;
+        case 2:
+          this.faceDiceRandom.style.backgroundImage =
+            "url(../image/dice-six-faces-two.svg)";
+          break;
+        case 3:
+          this.faceDiceRandom.style.backgroundImage =
+            "url(../image/dice-six-faces-three.svg)";
+          break;
+        case 4:
+          this.faceDiceRandom.style.backgroundImage =
+            "url(../image/dice-six-faces-four.svg)";
+          break;
+        case 5:
+          this.faceDiceRandom.style.backgroundImage =
+            "url(../image/dice-six-faces-five.svg)";
+          break;
+        case 6:
+          this.faceDiceRandom.style.backgroundImage =
+            "url(../image/dice-six-faces-six.svg)";
+          break;
+      }
+
+      if ( g.currentTurn === 1 ) {
+         p1.currentScore1 = currentScore
+      } else if ( g.currentTurn === 2 ) {
+        p2.currentScore2 = currentScore   
+
+
+
+      }
+
+  }
+
+  setTurnDice = function (g) {
+
+  
+    if( g.currentTurn === 1 ){
+
+    g.dice.classList.add('is-roll-dice-player-one')
+    
+    setTimeout(() => {
+      g.dice.classList.remove('is-roll-dice-player-one')
+    }, 2000);
+    
+
+  }else if( g.currentTurn === 2 ){
+    g.dice.classList.add('is-roll-dice-player-two')
+    setTimeout(() => {
+      g.dice.classList.remove('is-roll-dice-player-two')
+    }, 2000);
+  }
+
+
+  }
+
+
+
+
+
+}
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
-const dice_img = [dice-1.svg, dice-2.svg, dice-3.svg, dice-4.svg, dice-5.svg, dice-6.svg];
 
-const 
+const btnNewGame = document.getElementById('new__game')
+
+// instantiation of Class
+let newGame = new Game();
+let player1 = new PlayerOne();
+let player2 = new PlayerTwo();
+
+
+
+btnNewGame.addEventListener('click', (event) => {  
+  // Add Spedo
+  player1.setSpeudo1()
+  player2.setSpeudo2()
+  
+  newGame.currentTurn = 2;
+
+  // playerActive
+  player1.setPlayerOneActive(newGame, player1, player2)
+  player2.setPlayerTwoActive(newGame, player2, player1)
+
+  // btn Append
+  newGame.setBtnAppend(newGame)
+
+ 
+  
+})
+
+
+
+
+
+newGame.btnRollDice.addEventListener('click', newGame.setDebounce(function(e){
+  
+  newGame.setTurnDice(newGame);
+  newGame.setRandomNumber(newGame, player1, player2)
+
+}, 1000));
+
+
+
+
+
+
+
+
+
+
+// console.log(newGame.btnRollDice)
+
